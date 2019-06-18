129797abf3dc7991dc929019ea450d8ea232c709
omar573 <oomar573@gmail.com>
2019-06-18 10:17:37 -0400

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import skimage.transform as trans
 import numpy as np
 from keras.models import *
 from keras.layers import *
-from keras.optimizers import SGD
+from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 
