963697537b5d16ee4c59598a21c6c287e3a3d9b3
omar573 <oomar573@gmail.com>
2019-06-18 11:30:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,sa
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=5,callbacks=[model_checkpoint])
+ks=TensorBoard(log_dir="/your_full_path/logs/{}".format(time()), histogram_freq=1, write_graph=True, write_grads=True, batch_size=10)
+model.fit_generator(myGene,steps_per_epoch=300,epochs=5,callbacks=[model_checkpoint, ks])
 
 testGene = testGenerator("data/membrane/test")
 results = model.predict_generator(testGene,30,verbose=1)
